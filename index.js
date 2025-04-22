5d3a36ca7d933e209a4512db724df92d02b90120
WiperZ69 <yura.4etin.03@gmail.com>
2025-04-22 15:18:45 +0800

    do api 1, 2 and 3 hw

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 import { renderComments } from './modules/renderComments.js'
+import { updateComments } from './modules/comments.js'
+import { fetchComments } from './modules/api.js'
+import { initAddCommentListener } from './modules/initListeners.js'
 
+document.querySelector('.comments').innerHTML =
+    'Пожалуйста, подождите, загружаю комментарии...'
 
-renderComments()
+fetchComments().then((data) => {
+    updateComments(data)
+    renderComments()
+})
+
+initAddCommentListener(renderComments)
---
+++ modules/api.js
@@ -0,0 +1,54 @@
+const host = 'https://wedev-api.sky.pro/api/v1/chetin-yura'
+
+export const fetchComments = () => {
+    return fetch(host + '/comments')
+        .then((res) => {
+            return res.json()
+        })
+        .then((responseData) => {
+            const appComments = responseData.comments.map((comment) => {
+                return {
+                    name: comment.author.name,
+                    date: new Date(comment.date).toLocaleDateString('ru-RU', {
+                        day: '2-digit',
+                        month: '2-digit',
+                        year: 'numeric',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        hour12: false,
+                    }),
+                    text: comment.text,
+                    likes: comment.likes,
+                    isLiked: false,
+                }
+            })
+
+            return appComments
+        })
+}
+
+export const postComment = (text, name) => {
+    return fetch(host + '/comments', {
+        method: 'POST',
+        body: JSON.stringify({
+            text,
+            name,
+        }),
+    })
+        .then((response) => {
+            if (response.status === 500) {
+                throw new Error('Ошибка сервера')
+            }
+
+            if (response.status === 400) {
+                throw new Error('Неверный запрос')
+            }
+
+            if (response.status === 201) {
+                return response.json()
+            }
+        })
+        .then(() => {
+            return fetchComments()
+        })
+}
